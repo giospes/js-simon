7c674ab605a9d02147943ddb1152a7de5428c582
giospes <giovannijacopo.spes@gmail.com>
2023-03-21 16:27:12 +0100

    scelta dei numeri fatta

--- script.js
+++ script.js
@@ -5,24 +5,89 @@ function play(){
     const NUM_NUMERI = 5
     const startPage = document.getElementById("start-page")
     const numberPage = document.getElementById("number-page")
-    let numBlocks="", rdNUM;
+    const selectPage = document.getElementById("select-page")
+    const outputNumContainer = document.getElementById("num-selected-container")
+    
+
+    let numBlocks="", rdNUM =[];
 
     startPage.classList.add("d-none")
     numberPage.classList.remove("d-none")
 
     for(let i=0; i < NUM_NUMERI; i++){
-        rdNUM = Math.floor(Math.random()*100) + 1
+        rdNUM[i] = Math.floor(Math.random()*100) + 1
         numBlocks += 
         `
         <div class="card bg-secondary number d-flex justify-content-center align-items-center">
-            <span class ="fs-1 text-light">${rdNUM}</span>
+            <span class ="fs-1 text-light">${rdNUM[i]}</span>
         </div>
         `
     }
 
-    
+
     numberPage.innerHTML = numBlocks;
 
+    setTimeout(() => {
+        numberPage.classList.add("d-none");
+        selectPage.classList.remove("d-none");
+        outputNumContainer.classList.remove("d-none");
+    }, 5000);
+    printSquares(selectPage)
+
+
+    const checkBtn = document.getElementById('check-btn')
     
+    let selNumber = select(document.querySelectorAll('.gs-square'), checkBtn)
+        
+    checkBtn.addEventListener('click',  () => {
+        selectPage.classList.add("d-none")
+        checkBtn.classList.add("d-none")
+    })
+    
+    
+    
+
+
+   
+    
+
+}
+
+
+
+function printSquares(squareContainer){
+    let square, squares = [];
+    squareContainer.innerHTML =''
+    for(let i = 1; i<=100; i++){
+        square = document.createElement("div");
+        square.innerHTML = `<span>${i}</span>`;
+        square.className = "gs-square border bg-primary d-flex align-items-center justify-content-center"
+        squares[i] = square;
+    }
+    squares.forEach(s => squareContainer.appendChild(s));
+}
+
+function select(sqrs, checkBtn){
+    const output = document.getElementById("num-selected")
+    let counter = 0, input =[];
+    sqrs.forEach((sqr, index) => {
+        
+        sqr.addEventListener('click', function(){
+            if(counter<5){
+                input[counter] = parseInt(sqrs[index].querySelector("span").textContent)
+                console.log(input)
+                output.innerHTML +=  input[counter] + ' '
+                counter++;
+            } 
+            if(input.length === 5){
+                checkBtn.classList.remove('d-none');
+                return input;
+            } 
+        })
+        
+
+    })
+
+     
 
 }
